2bf1434dc2a40edbcf5ce2d17e578431ddea96ff
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-02-21 20:02:48 +0530

    initial setup for project

---
+++ FlickerBrowser/app/src/main/java/com/android/ashdeep/flickerbrowser/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.android.ashdeep.flickerbrowser.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
